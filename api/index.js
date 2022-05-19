4d932f380b55393b6bb09f303e1a65fa53be5f59
mtaha1990 <mohammad.taha.1990@gmail.com>
2022-05-19 14:48:09 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,9 +22,4 @@ http.createServer((req,res)=>{
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.end();
-});
-
-
-http.listen(3500, () => {
-  console.log('listening...');
-});
+}).listen(6666);
